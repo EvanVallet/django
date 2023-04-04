026bad515bfded4ce6b859f9ffe03034cf744d69
EvanVallet <118521328+EvanVallet@users.noreply.github.com>
2023-04-04 11:23:44 +0200

    Add files via upload

---
+++ firstproject/myfirstapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'myfirstapp/index.html')
+
+def formulaire(request):
+    return render(request, 'myfirstapp/formulaire.html')
+
+def Bonjour(request):
+    nom = request.GET["nom"]
+    return render(request, 'myfirstapp/Bonjour.html',{"nom":nom})
